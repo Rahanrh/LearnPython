88e5c626d65931d2209a14d3fcafbe3894d528d9
ichak or raha <ichanuranugraha343214@gmail.com>
2023-01-16 21:12:03 +0700

    perbaikan

--- Fungsi Operator/Identity Operators.py
+++ Fungsi Operator/Identity Operators.py
@@ -7,4 +7,4 @@ print(l1 is l2)  # return true because both are refer same object
 l3 = [1, 2, 3]
 
 print(l1 is l3)  # return False because lists are mutable in Python
-print(l1 is not l3)
+print(l1 is not l2)
